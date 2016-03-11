14e5de405a315471d8bd7f708fb378207e747d6c
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-03-11 11:35:27 -0300

    Simplify and show appropriate types

--- src/components/route-card.js
+++ src/components/route-card.js
@@ -41,7 +41,7 @@ const RouteCard = ({alt, transitiveData, travelTime, oldTravelTime}) => {
                       color: (s.color || 'inherit')
                     }}
                     >
-                    <i className='fa fa-bus'></i> {s.name}
+                    <i className={`fa fa-${s.type}`}></i> {s.name}
                   </span>
                 )
               })}
@@ -60,32 +60,24 @@ function extractRelevantTransitiveInfo ({journeys, patterns, routes, stops}) {
       return j.segments
         .filter(s => !!s.pattern_id)
         .map(s => {
-          const seg = {
-            type: s.type.toLowerCase()
-          }
-          seg.color = '#333'
-
-          if (s.from_stop_index) seg.from = stops[s.from_stop_index].stop_name
-          if (s.to_stop_index) seg.to = stops[s.to_stop_index].stop_name
-
-          if (s.from) {
-            if (s.from.stop_id) seg.from = stops[parseInt(s.from.stop_id, 10)].stop_name
-            else seg.start = true
-          }
-          if (s.to) {
-            if (s.to.stop_id) seg.to = stops[parseInt(s.to.stop_id, 10)].stop_name
-            else seg.end = true
-          }
-
+          const seg = {}
           const route = routes[parseInt(patterns[parseInt(s.pattern_id, 10)].route_id, 10)]
           const color = Color(`#${route.route_color}`)
           seg.name = route.route_long_name
           seg.backgroundColor = color.rgbaString()
           seg.color = color.light() ? '#000' : '#fff'
+          seg.type = typeToIcon[route.route_type]
 
           return seg
         })
     })
+}
+
+const typeToIcon = {
+  0: 'subway',
+  1: 'subway',
+  2: 'train',
+  3: 'bus'
 }
 
 export default RouteCard
