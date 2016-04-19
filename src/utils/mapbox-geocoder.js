89f03cae21e4859bdd4b8f7ba7fb0908191d8c6b
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-04-19 14:43:59 -0300

    Dont use poi

--- src/utils/mapbox-geocoder.js
+++ src/utils/mapbox-geocoder.js
@@ -7,18 +7,15 @@ const geocoder = mapbox.geocoder('mapbox.places')
 export default function search (api_key, text, {boundary, focusLatlng, format} = {}) {
   if (!text) return Promise.resolve([])
 
-  console.log(focusLatlng)
-
   return new Promise((resolve, reject) => {
     geocoder.query({
       country: boundary.country,
       proximity: focusLatlng,
       query: text,
-      types: ['address', 'locality', 'neighborhood', 'place', 'poi']
+      types: ['address', 'locality', 'neighborhood', 'place']
     }, (err, results) => {
       if (err) return reject(err)
       if (!results) return resolve()
-      console.log(results)
       resolve(results.results)
     })
   })
