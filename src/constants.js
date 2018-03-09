1d01e22fc9eaf5d1b09747b93a8e5c9f903787a4
Trevor Gerhardt <trevorgerhardt@gmail.com>
2018-03-09 15:49:56 +0800

    fix(mapbox): Use https geocoding endpoint

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,4 @@ export const ACCESSIBILITY_IS_LOADING = 'accessibility-is-loading'
 
 // URLS
 export const MAPBOX_GEOCODING_URL =
-  'http://api.mapbox.com/geocoding/v5/mapbox.places'
+  'https://api.mapbox.com/geocoding/v5/mapbox.places'
