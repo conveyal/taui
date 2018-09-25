c22a5df9917402cd041919915201618b683d966b
Trevor Gerhardt <trevorgerhardt@gmail.com>
2018-09-25 17:43:54 +0800

    fix(scrollbar): Fix scrollbar gap display

--- src/selectors/points-of-interest.js
+++ src/selectors/points-of-interest.js
@@ -5,18 +5,23 @@ export default createSelector(
   state => state.data.pointsOfInterest,
   featureCollection =>
     (featureCollection
-      ? featureCollection.features.map(feature => ({
-        label: feature.properties.name,
-        value: `poi-${feature.properties.name}-${feature.geometry.coordinates.join(',')}`,
-        feature: {
-          ...feature,
-          properties: {
-            ...feature.properties,
-            label: feature.properties.name,
-            'marker-color': '#0b2b40',
-            'marker-size': 'small'
+      ? featureCollection.features.map(feature => {
+          const p = feature.properties
+          const label = p.label || p.name || p.Name
+          return {
+            label,
+            value: `poi-${label}-${feature.geometry.coordinates.join(',')}`,
+            feature: {
+              ...feature,
+              properties: {
+                ...p,
+                label,
+                'marker-color': '#0b2b40',
+                'marker-size': 'small'
+              }
+            }
           }
-        }
-      }))
-      : [])
+        })
+      : []
+    )
 )
